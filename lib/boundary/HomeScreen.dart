86244311b97343d8ce4cd2f099c515c570817f08
Xueyao <c.xueyao00>
2021-03-31 03:24:23 +0800

    Updated HomeScreen.dart with live location tracking, updated assets, updated yaml

--- lib/boundary/HomeScreen.dart
+++ lib/boundary/HomeScreen.dart
@@ -1,10 +1,12 @@
 import 'dart:typed_data';
-
 import 'package:dropdownfield/dropdownfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:denguego/controller/ClusterManager.dart';
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:location/location.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,6 +14,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  StreamSubscription _locationSubscription;
+  Location _locationTracker = Location();
+  List<Marker> allMarkers = [];
+  Circle circle;
+  GoogleMapController _controller;
+  static final CameraPosition initialLocation = CameraPosition(
+      target: LatLng(1.290270, 103.851959),
+      zoom: 15);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,22 +35,29 @@ class _HomeScreenState extends State<HomeScreen> {
           //_boxes(),
         ],
       ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.location_searching),
+            onPressed: () {
+              getCurrentLocation();
+            })
     );
   }
-}
+
 
 Widget _buildGoogleMap(BuildContext context) {
   return Container(
     height: MediaQuery.of(context).size.height,
     width: MediaQuery.of(context).size.width,
     child: GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition: CameraPosition(
-        target: LatLng(1.3521, 103.8198),
-        zoom: 10,
+        mapType: MapType.normal,
+        initialCameraPosition: initialLocation,
+        markers: Set.from(allMarkers),
+        circles: Set.of((circle != null) ? [circle] : []),
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        }
       ),
-    ),
-  );
+    );
 }
 
 Widget _buildSearch(List<String> clusters) {
@@ -72,3 +91,81 @@ Widget _buildSearch(List<String> clusters) {
     ),
   );
 }
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    // buffer marker to simulate dengue cluster
+    allMarkers.add(Marker(
+        markerId: MarkerId("DengueCluster1"),
+        draggable: false,
+        zIndex: 2,
+        flat: true,
+        anchor: Offset(0.5, 0.5),
+        position: LatLng(1.3491, 103.6789)));
+  }
+
+  Future<Uint8List> getMarker() async {
+    ByteData byteData = await DefaultAssetBundle.of(context).load("assets/arrow.png");
+    return byteData.buffer.asUint8List();
+  }
+
+
+  void updateMarkerAndCircle(LocationData newLocalData, Uint8List imageData) {
+    LatLng latLng = LatLng(newLocalData.latitude, newLocalData.longitude);
+    this.setState(() {
+      allMarkers.add(Marker(
+          markerId: MarkerId("home"),
+          position: latLng,
+          rotation: newLocalData.heading,
+          draggable: false,
+          zIndex: 2,
+          flat: true,
+          anchor: Offset(0.5, 0.5),
+          icon: BitmapDescriptor.fromBytes(imageData)));
+      circle = Circle(
+          circleId: CircleId("arrow"),
+          radius: newLocalData.accuracy,
+          zIndex: 1,
+          strokeColor: Colors.blue,
+          center: latLng,
+          fillColor: Colors.blue.withAlpha(70));
+    });
+  }
+
+  void getCurrentLocation() async {
+    try {
+
+      Uint8List imageData = await getMarker();
+      var location = await _locationTracker.getLocation();
+
+      updateMarkerAndCircle(location, imageData);
+
+      if (_locationSubscription != null){
+        _locationSubscription.cancel();
+      }
+
+      _locationSubscription = _locationTracker.onLocationChanged().listen((newLocalData) {
+        if (_controller != null) {
+          _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+              bearing: 0,
+              target: LatLng(newLocalData.latitude, newLocalData.longitude),
+              tilt:0,
+              zoom:16)));
+          updateMarkerAndCircle(newLocalData, imageData);
+        }
+      });
+    }
+
+    on PlatformException catch (e) {
+      if (e.code == 'PERMISSION DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+
+
+
+
+}}
